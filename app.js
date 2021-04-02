5d940cfe9eafd09e6f68e0b81718fc3ff71133c0
rolandmontalvan <rolandfilho@gmail.com>
2021-04-02 10:33:58 -0300

    nodemon-e-exclusao-do-bodyparser

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
 const rotaprodutos = require('./routes/produtos');
 const rotapedidos = require('./routes/pedidos');
+
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false })); //dados simples
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false })); //dados simples
+app.use(express.json());
 app.use('/produtos', rotaprodutos);
 app.use('/pedidos', rotapedidos);
+
 // tratamento para quando a rota não for encontrada
 app.use((req, res, next) => {
     const erro = new Error('Não encontrado');
